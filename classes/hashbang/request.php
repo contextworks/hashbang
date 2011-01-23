cedd68a8dde2f1befbbca5f388b4ab02f9811f22
Woody Gilk <woody.gilk@gmail.com>
2011-01-22 23:15:06 -0600

    Initial creation of hashbang module

---
+++ classes/hashbang/request.php
@@ -0,0 +1,37 @@
+<?php defined('SYSPATH') or die('No direct script access.');
+
+class Hashbang_Request extends Kohana_Request {
+
+	public static $hashbang_redirect_get = FALSE;
+
+	public static $hashbang_redirect_code = 301;
+
+	public static function instance( & $uri = TRUE)
+	{
+		if ( ! Request::$instance)
+		{
+			if (isset($_GET['_escaped_fragment_']))
+			{
+				// Capture the hashbang
+				$redirect = $_GET['_escaped_fragment_'];
+
+				if (Request::$hashbang_include_get)
+				{
+					// Append all GET parameters and remove the hashbang
+					$redirect .= URL::query(array('_escaped_fragment_' => NULL));
+				}
+			}
+		}
+
+		// Get the instance
+		$instance = parent::instance($uri);
+
+		if (isset($redirect) AND ! Request::$is_ajax)
+		{
+			$instance->redirect($redirect, Request::$hashbang_redirect_code);
+		}
+
+		return $instance;
+	}
+
+} // End Hashbang_Request
